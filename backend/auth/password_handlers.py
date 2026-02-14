0f36203c61c419cd16d17055b2f07bcecff2bb51
Viky <vikyy905@gmail.com>
2026-02-14 20:46:22 +0100

    Implement password hashing and verification functions

--- backend/auth/password_handlers.py
+++ backend/auth/password_handlers.py
@@ -1,1 +1,16 @@
 import bcrypt
+
+def hash_password(raw_pass: str) -> str:
+    try:
+        salt = bcrypt.gensalt()
+        bytes = raw_pass.encode()
+        hashed_password = bcrypt.hashpw(bytes, salt)
+        return hashed_password.decode()
+    except Exception as e:
+        pass 
+
+def check_password(entered_pass: str, hashed_pass: str) -> bool:
+    try:
+        return bcrypt.checkpw(entered_pass.encode(), hashed_pass.encode())
+    except Exception as e:
+        pass 
