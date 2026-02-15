91e6c40eefb2a79e301e364f4b24acc002bdc0f5
RomanovYehor <yeghor.romanov@gmail.com>
2026-02-15 21:06:00 +0200

    add postgre & redis methods for auth, improve endpoints db commit on exception logic

--- backend/routers/auth_router.py
+++ backend/routers/auth_router.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from services import AuthService
 from postgre import get_session_depends
-from DTOs import LoginBody, RegisterBody
+from DTOs import LoginBody, RegisterBody, JWTsResponse
 
 auth = APIRouter()
 
@@ -11,24 +11,34 @@ auth = APIRouter()
 async def login(
     login_creds: LoginBody,
     postgre_session: AsyncSession = Depends(get_session_depends)
-):
+) -> JWTsResponse:
+    exception_occured = False
     try:
-        auth_service: AuthService = await AuthService.create(postgre_session)
-        return await auth_service.login(login_creds)
+        auth_service: AuthService = await AuthService[AuthService].create(postgre_session)
+        return await auth_service.register(login_creds)
+    except Exception as e:
+        exception_occured = True
+        raise e from e
     finally:
-        auth_service.close()
+        if not exception_occured:
+            await auth_service.close(commit=True)
 
 
 @auth.post("/register")
 async def register(
     register_creds: RegisterBody,
     postgre_session: AsyncSession = Depends(get_session_depends)
-):
+) -> JWTsResponse:
+    exception_occured = False
     try:
         auth_service: AuthService = await AuthService[AuthService].create(postgre_session)
         return await auth_service.register(register_creds)
+    except Exception as e:
+        exception_occured = True
+        raise e from e
     finally:
-        auth_service.close()
+        if not exception_occured:
+            await auth_service.close(commit=True)
 
 
 @auth.post("/logout")
--- backend/services/postgre_service/postgre_service.py
+++ backend/services/postgre_service/postgre_service.py
@@ -1,14 +1,30 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from postgre import Base, User
+
+from typing import TypeVar
+
+M = TypeVar("M", bound=Base)
+
 
 class PostgreService():
     def __init__(self, session: AsyncSession):
         self.__sesion = session
 
     async def commit(self) -> None:
-        pass
+        await self.__sesion.commit()
 
     async def flush(self) -> None:
+        await self.__sesion.flush()
+
+    async def flush_models(self, *models: Base) -> None:
+        self.__sesion.add_all(models)
+        await self.flush()
+
+    async def create_model(self, Model: M, **kwargs) -> M:
+        return Model(**kwargs)
+    
+    async def get_user_by_username(self, username: str) -> User | None:
         pass
 
-    async def close(self, commit: bool) -> None:
+    async def get_user_by_email(self, email: str) -> User | None:
         pass
