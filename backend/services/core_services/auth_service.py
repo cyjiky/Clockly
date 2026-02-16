a8677a414b270b5ed720d0a3238ec2a16e0acd9e
Viky <vikyy905@gmail.com>
2026-02-16 18:13:02 +0100

    Implement registration and login functionality in AuthService

--- backend/services/core_services/auth_service.py
+++ backend/services/core_services/auth_service.py
@@ -2,12 +2,44 @@ from services import CoreServiceBase
 
 from DTOs import LoginBody, RegisterBody
 
+from postgre import User
+from typing import Optional
+import auth as auth_utils
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from uuid import uuid4
+import os
+
 class AuthService(CoreServiceBase):
-    async def login(self, creds: LoginBody):
-        pass
 
     async def register(self, cred: RegisterBody):
-        pass
+        auth_utils.validate_email(cred.email)
+        auth_utils.validate_password(cred.password)
+
+        if await self._PostgresService.get_user_by_username_or_email(
+            username=cred.username, 
+            email=cred.email):
+            raise ValueError
+
+        new_user = User(
+            user_id=str(uuid4()),
+            username = cred.username, 
+            email = cred.email, 
+            password_hash = auth_utils.hash_password(cred.password)
+        )
+        await self._PostgresService(new_user)
+        return None
+
+
+    async def login(self, cred: LoginBody):
+        potencial_user:User = await self._PostgreService.get_user_by_username_or_email(
+            username=cred.username, 
+            email=None)
+        
+        if not potencial_user:
+            raise ValueError
+        
+        return None 
 
     async def logout(self):
         pass
