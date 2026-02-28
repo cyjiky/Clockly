c3cfd0112da9767d4ac2d761de6c8b8be5cfcd39
RomanovYehor <yeghor.romanov@gmail.com>
2026-02-28 15:13:54 +0200

    feat: add range defining function, get by range events and tasks function with sql query

--- backend/utils/dates_utils.py
+++ backend/utils/dates_utils.py
@@ -1,20 +1,36 @@
 import datetime
 import calendar
-
+from typing import Tuple
 from app_types import TimeLineEnum
 
-def map_nearest_range(curr_date: datetime._Date, range: TimeLineEnum) -> datetime:
-    match range:
+StartDate = datetime.datetime
+EndDate = datetime.datetime
+
+
+def map_nearest_range(
+    curr_date: datetime.datetime, timerange: TimeLineEnum
+) -> Tuple[StartDate, EndDate]:
+    match timerange:
         case TimeLineEnum.DAY:
-            return curr_date + datetime.timedelta(days=TimeLineEnum.DAY.value)
+            return curr_date, curr_date + datetime.timedelta(
+                days=TimeLineEnum.DAY.value
+            )
         case TimeLineEnum.THREE_DAYS:
-            return curr_date + datetime.timedelta(days=TimeLineEnum.THREE_DAYS.value)
+            return curr_date, curr_date + datetime.timedelta(
+                days=TimeLineEnum.THREE_DAYS.value
+            )
         case TimeLineEnum.WEEK:
-            return curr_date + datetime.timedelta(days=TimeLineEnum.WEEK.value)
+            return curr_date, curr_date + datetime.timedelta(
+                days=TimeLineEnum.WEEK.value
+            )
         case TimeLineEnum.MONTH:
             curr_year = curr_date.year
             curr_month = curr_date.month
             curr_days_passed = curr_date.day
             n_days = calendar.monthrange(curr_year, curr_month)
-            
-            return curr_date + datetime.timedelta(days=n_days-curr_days_passed)
+
+            return datetime.datetime(
+                year=curr_date.year, month=curr_date.month, day=1
+            ), curr_date.date + datetime.timedelta(
+                days=n_days - curr_days_passed
+            )
