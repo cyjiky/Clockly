0fd8accb5faabe5b3066a0433db8d4a48d7fcf80
Viky <vikyy905@gmail.com>
2026-03-27 22:19:46 +0100

    Add avatar component to frontend app

--- frontend/components/calendar_comp/repeat.tsx
+++ frontend/components/calendar_comp/repeat.tsx
@@ -1,11 +1,25 @@
 import { View, Text } from 'react-native';
+import { Bold, Italic, Underline } from 'lucide-react-native';
+import { ToggleGroup, ToggleGroupIcon, ToggleGroupItem } from '@/components/ui/toggle-group';
+import * as React from 'react';
+
+// TODO 
 
 export default function Repeat() {
+    const [value, setValue] = React.useState<string[]>([]);
     return (
         <View className="flex-1 items-center justify-center bg-slate-100">
-            <Text className="text-black text-2xl font-extrabold tracking-widest text-center">
-                Repeat Selection!
-            </Text>
+            <ToggleGroup value={value} onValueChange={setValue} variant='outline' type='multiple'>
+                <ToggleGroupItem isFirst value='bold' aria-label='Toggle bold'>
+                    <ToggleGroupIcon as={Bold} />
+                </ToggleGroupItem>
+                <ToggleGroupItem value='italic' aria-label='Toggle italic'>
+                    <ToggleGroupIcon as={Italic} />
+                </ToggleGroupItem>
+                <ToggleGroupItem isLast value='strikethrough' aria-label='Toggle strikethrough'>
+                    <ToggleGroupIcon as={Underline} />
+                </ToggleGroupItem>
+            </ToggleGroup>
         </View>
     )
 }
