bee7bcf34c9cc49a197c88cbdf3065a5209554e4
RomanovYehor <yeghor.romanov@gmail.com>
2026-04-30 23:09:09 +0300

    Move .env conf variables to pydantic-settings class in conf.py

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,34 @@
+from pydantic import Field, PostgresDsn
 from pydantic_settings import BaseSettings
+from typing import Literal
 
 class Settings(BaseSettings):
-    # TODO ENV
+    app_mode: Literal["prod", "test"]
 
+    # auth
+    jwt_secret: str = Field(validation_alias="JWT_SECRET_KEY")
 
     # logging
     logging_filename: str = "logs.log"
     logger_name: str = "clockly_logger"
     LOGGING_FORMAT: str = "%(levelname)s (%(asctime)s): %(message)s"
+
+    # postgres
+    postgres_dsn_prod: PostgresDsn = "postgresql+asyncpg://database:password@localhost:5432/prod"
+    postgres_dsn_test: PostgresDsn = "postgresql+asyncpg://database:password@localhost:5432/test"
+
+    # app
+    pagination: int = 30
+
+    @classmethod
+    def get_postgres_dsn(cls) -> PostgresDsn:
+        """Returns postgresql DSN regarding to the app_mode variable"""
+
+        match cls.app_mode: 
+            case "prod":
+                return cls.postgres_dsn_prod
+            case "test":
+                return cls.postgres_dsn_test
+
 
 settings = Settings()
