1c952575ccbccc3b4ae01291c3c346f07c391696
RomanovYehor <yeghor.romanov@gmail.com>
2026-05-01 22:54:42 +0300

    Create non tested draft of objects range generator

--- backend/services/core_services/export_service.py
+++ backend/services/core_services/export_service.py
@@ -3,7 +3,7 @@ from typing import AsyncGenerator
 
 from .calendar_shared_methods_service_base import CoreServiceBaseSharedMethods
 
-class VisualizationService(CoreServiceBaseSharedMethods):
+class ExportService(CoreServiceBaseSharedMethods):
     async def csv_historic_data_generator(self, start_date: datetime, end_date: datetime) -> AsyncGenerator[None, str]:
         pass
 
