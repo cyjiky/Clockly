a2cfd3431315a696ae6af60e339b0c3f6e14ea1b
RomanovYehor <yeghor.romanov@gmail.com>
2026-05-30 21:29:42 +0300

    Fix compose.yaml configuation

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -3,4 +3,12 @@ const { withNativeWind } = require('nativewind/metro');
  
 const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
  
+config.watcher = {
+    ...config.watcher,
+    additionalWatchers: [],
+    watchman: false,
+    usePolling: true,
+    interval: 1000,
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
